2fb3083880559048107baa902a38c662d14d866a
GhemesPaul <ghemespaul@gmail.com>
2018-07-27 13:56:19 +0300

    test comment

--- jbugs-persistence/src/main/java/edu/msg/ro/persistence/user/entity/User.java
+++ jbugs-persistence/src/main/java/edu/msg/ro/persistence/user/entity/User.java
@@ -14,7 +14,7 @@ public class User {
     private String username;
     private String password;
     private String status;
-
+     //test
     public long getIdUser() {
         return idUser;
     }
